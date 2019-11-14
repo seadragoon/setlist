a45c90bc1513eed655f6811cb3db5742ca59b3de
NaraKeisuke <nara.keisuke@tecotec.co.jp>
2019-11-14 14:15:58 +0900

    ログインがなされていなければ実行できないアクション（ルート）をmiddleware => authを挟むようにして権限管理 権限が無い場合はabort('403')で403エラー画面を表示させる

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,12 @@
 |
 */
 
+// indexページ
 Route::redirect('/', 'index');
 Route::get('/index', 'IndexController@index');
 
+// TODO: 本番に上げる際にはコメントアウトする
 Route::get('/generateMaster', 'GenerateMasterController@index');
-
-Route::get('/artists/add', 'ArtistsController@add');
-Route::get('/songs/add', 'SongsController@add');
 
 Route::get('/events/search', 'EventsController@search');
 Route::get('/artists/search', 'ArtistsController@search');
@@ -26,10 +25,21 @@ Route::get('/songs/search', 'SongsController@search');
 Route::get('/aggregate', 'AggregateController@index');
 Route::get('/aggregate/show/{artist_id}', 'AggregateController@show');
 
-Route::resource('artists', 'ArtistsController', ['only' => ['index', 'show', 'edit', 'update', 'destroy', 'store']]);
-Route::resource('songs', 'SongsController', ['only' => ['index', 'show', 'edit', 'update', 'destroy', 'store']]);
-Route::resource('events', 'EventsController', ['only' => ['index', 'show', 'edit', 'create', 'destroy', 'store']]);
+Route::resource('artists', 'ArtistsController', ['only' => ['index', 'show']]);
+Route::resource('songs', 'SongsController', ['only' => ['index', 'show']]);
+Route::resource('events', 'EventsController', ['only' => ['index', 'show']]);
 
+// twitterログイン
 Route::get('auth/twitter', 'Auth\SocialAuthController@redirectToProvider');
 Route::get('auth/twitter/callback', 'Auth\SocialAuthController@handleProviderCallback');
 Route::get('auth/twitter/logout', 'Auth\SocialAuthController@logout');
+
+// ログインが必要なルート
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/artists/add', 'ArtistsController@add');
+    Route::get('/songs/add', 'SongsController@add');
+    
+    Route::resource('artists', 'ArtistsController', ['only' => ['edit', 'update', 'destroy', 'store']]);
+    Route::resource('songs', 'SongsController', ['only' => ['edit', 'update', 'destroy', 'store']]);
+    Route::resource('events', 'EventsController', ['only' => ['edit', 'create', 'destroy', 'store']]);
+});
