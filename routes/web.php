1d81af3e350c00f7b65c6c49bb38b7781f487624
NaraKeisuke <nara.keisuke@tecotec.co.jp>
2019-11-14 18:50:13 +0900

    routeの設定を整理して変更、route-nameを設定

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,20 @@
 Route::redirect('/', 'index');
 Route::get('/index', 'IndexController@index');
 
-// TODO: 本番に上げる際にはコメントアウトする
-Route::get('/generateMaster', 'GenerateMasterController@index');
-
-Route::get('/events/search', 'EventsController@search');
-Route::get('/artists/search', 'ArtistsController@search');
-Route::get('/songs/search', 'SongsController@search');
-
-Route::get('/aggregate', 'AggregateController@index');
-Route::get('/aggregate/show/{artist_id}', 'AggregateController@show');
-
-Route::resource('artists', 'ArtistsController', ['only' => ['index', 'show']]);
-Route::resource('songs', 'SongsController', ['only' => ['index', 'show']]);
-Route::resource('events', 'EventsController', ['only' => ['index', 'show']]);
+// index
+Route::get('/artists', 'ArtistsController@index')->name('artists.index');
+Route::get('/songs', 'SongsController@index')->name('songs.index');
+Route::get('/events', 'EventsController@index')->name('events.index');
+Route::get('/aggregate', 'AggregateController@index')->name('aggregate.index');
+// show
+Route::get('/artists/{artist_id}', 'ArtistsController@show')->name('artists.show')->where('artist_id', '[0-9]+');
+Route::get('/songs/{song_id}', 'SongsController@show')->name('songs.show')->where('song_id', '[0-9]+');
+Route::get('/events/{event_id}', 'EventsController@show')->name('events.show')->where('event_id', '[0-9]+');
+Route::get('/aggregate/show/{artist_id}', 'AggregateController@show')->name('aggregate.show')->where('artist_id', '[0-9]+');
+// search
+Route::get('/artists/search', 'ArtistsController@search')->name('artists.search');
+Route::get('/songs/search', 'SongsController@search')->name('songs.search');
+Route::get('/events/search', 'EventsController@search')->name('events.search');
 
 // twitterログイン
 Route::get('auth/twitter', 'Auth\SocialAuthController@redirectToProvider');
@@ -36,10 +37,13 @@ Route::get('auth/twitter/logout', 'Auth\SocialAuthController@logout');
 
 // ログインが必要なルート
 Route::group(['middleware' => 'auth'], function() {
-    Route::get('/artists/add', 'ArtistsController@add');
-    Route::get('/songs/add', 'SongsController@add');
+    Route::get('/artists/add', 'ArtistsController@add')->name('artists.add');
+    Route::get('/songs/add', 'SongsController@add')->name('songs.add');
     
     Route::resource('artists', 'ArtistsController', ['only' => ['edit', 'update', 'destroy', 'store']]);
     Route::resource('songs', 'SongsController', ['only' => ['edit', 'update', 'destroy', 'store']]);
     Route::resource('events', 'EventsController', ['only' => ['edit', 'create', 'destroy', 'store']]);
+    
+    // TODO: 本番に上げる際にはコメントアウトする
+    Route::get('/generateMaster', 'GenerateMasterController@index');
 });
