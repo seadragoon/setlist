8e7e83a189a22522bc415d6499e7a042637709ee
seadragoon <seadragoon18@gmail.com>
2019-11-22 04:48:45 +0900

    utf8 -> utf8mb4 レイアウト調整(データが無い場合の分岐)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,34 +13,39 @@
             <h6>・このサイトについて</h6>
             アーティストのライブセットリストを登録・集計するためのサイトです。<br>
             <br>
-            <h4>直近のイベント一覧({{ $events->currentPage() }}/{{ $events->lastPage() }})</h4>
-            <div class="d-none d-sm-flex">
-                <div class="col-sm-5">イベント名</div>
-                <div class="col-sm-3">日付</div>
-                <div class="col-sm-4">会場名</div>
-            </div>
-            <div class="mx-2">
-                @foreach ($events as $event)
-                    @if($loop->iteration == 1)
-                    <div class="row border-top border-bottom py-2">
-                    @else
-                    <div class="row border-bottom py-2">
-                    @endif
-                        <div class="col-sm-5 col-12">
-                            {{ link_to_route('events.show', $event->name, $event->event_id, ['class' => 'btn-default']) }}
+            @if($events->total() > 0)
+                <h4>直近のイベント一覧({{ $events->currentPage() }}/{{ $events->lastPage() }})</h4>
+                <div class="d-none d-sm-flex">
+                    <div class="col-sm-5">イベント名</div>
+                    <div class="col-sm-3">日付</div>
+                    <div class="col-sm-4">会場名</div>
+                </div>
+                <div class="mx-2">
+                    @foreach ($events as $event)
+                        @if($loop->iteration == 1)
+                        <div class="row border-top border-bottom py-2">
+                        @else
+                        <div class="row border-bottom py-2">
+                        @endif
+                            <div class="col-sm-5 col-12">
+                                {{ link_to_route('events.show', $event->name, $event->event_id, ['class' => 'btn-default']) }}
+                            </div>
+                            <div class="col-sm-3 col-12">
+                                {{ date('Y年m月d日',  strtotime($event->datetime)) }}
+                            </div>
+                            <div class="col-sm-4 col-12">
+                                {{ $event->venue_name }}
+                            </div>
                         </div>
-                        <div class="col-sm-3 col-12">
-                            {{ date('Y年m月d日',  strtotime($event->datetime)) }}
-                        </div>
-                        <div class="col-sm-4 col-12">
-                            {{ $event->venue_name }}
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-            <!-- page control -->
-            <br>
-            {!! $events->render() !!}
+                    @endforeach
+                </div>
+                <!-- page control -->
+                <br>
+                {!! $events->render() !!}
+            @else
+                <h4>直近のイベント一覧</h4>
+                <p>イベントが登録されていません。</p>
+            @endif
         </div>
     </div>
 
